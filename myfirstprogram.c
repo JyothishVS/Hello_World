eb80382d2a72edb7523a19e0873402325de663e4
Jyothish Vallivedusudhakar <89015769+JyothishVS@users.noreply.github.com>
2021-11-25 00:42:04 +0530

    Program Created

---
+++ myfirstprogram.c
@@ -0,0 +1,22 @@
+// My first program - I had written in first year of my college
+// C Program - It outputs or display the message "Hello, World!"
+// Header file and main function
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello, World!");
+    return 0;
+}
+
+// C++ program to display "Hello World"
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!";
+    return 0;
+}
+*/
